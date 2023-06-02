a81f5decbed0f0ac0c3b4ffb1c0824c9fde558b1
unknown <mborkhuis@st.noorderpoort.nl>
2023-06-02 13:03:18 +0200

    init

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('/', 'welcome')->name('home');
+    Route::view('/home', 'home')->name('home');
+    Route::view('/test', 'test')->name('test');
 });
 
-Route::get('/home', function () {
-    return view('home');
-})->middleware(['auth', 'verified'])->name('home');
-
+// /profile: This route is accessed with the HTTP GET method and is handled by the edit method of the ProfileController. It is named as profile.edit.
+// /profile: This route is accessed with the HTTP PATCH method and is handled by the update method of the ProfileController. It is named as profile.update.
+// /profile: This route is accessed with the HTTP DELETE method and is handled by the destroy method of the ProfileController. It is named as profile.destroy.
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
